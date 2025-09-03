b39c7a96f051e54c87e75f025c058a4f3fd48740
MoojMidge <56883549+MoojMidge@users.noreply.github.com>
2025-09-03 12:58:31 +0900

    Add ViewManager
    - Updated to be more self-contained and work better with unsupported skins
    - TODO: Add support for setting default sort order and sort direction
    
    Fix content type not being set to episodes
    
    - Fix #586, #589
    
    Update for restructure of xbmc_plugin
    
    - Only set view mode if directory items successfully added
    
    Fix preselect on view_manager view lists
    
    Update to match new setup wizard
    
    Update for reorganised/renamed constants fca610c
    
    Update for updated XbmcContext.apply_content 8a8247a
    
    Update for new localize and logging methods
    
    Update to fix setting view mode not working if container is still updating
    
    Update to handle sort method and order and workaround #1243

--- resources/lib/youtube_plugin/kodion/constants/__init__.py
+++ resources/lib/youtube_plugin/kodion/constants/__init__.py
@@ -175,6 +175,8 @@ PAGE = 'page'
 PLAYLIST_IDS = 'playlist_ids'
 SCREENSAVER = 'screensaver'
 SEEK = 'seek'
+SORT_DIR = 'sort_dir'
+SORT_METHOD = 'sort_method'
 START = 'start'
 VIDEO_IDS = 'video_ids'
 
@@ -307,6 +309,8 @@ __all__ = (
     'PLAYLIST_IDS',
     'SCREENSAVER',
     'SEEK',
+    'SORT_DIR',
+    'SORT_METHOD',
     'START',
     'VIDEO_IDS',
 
