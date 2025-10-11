9292a194490ea03f451b9c8a6275292d1b068e9a
MoojMidge <56883549+MoojMidge@users.noreply.github.com>
2025-10-11 15:18:51 +1100

    Allow restoring current container position

--- resources/lib/youtube_plugin/kodion/monitors/service_monitor.py
+++ resources/lib/youtube_plugin/kodion/monitors/service_monitor.py
@@ -384,6 +384,8 @@ class ServiceMonitor(xbmc.Monitor):
                 elif position == 'previous':
                     position = ui.get_container_info(CURRENT_ITEM, container_id)
                     offset -= 1
+                elif position is True:
+                    position = ui.get_container_info(CURRENT_ITEM, container_id)
                 try:
                     position = int(position)
                 except (TypeError, ValueError):
